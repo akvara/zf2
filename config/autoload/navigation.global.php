fa10d74ef6f1984db7aba12f2000dba9ecc8fb7e
Andrius K <akvara@gmail.com>
2013-09-13 17:04:35 +0300

    paginator

--- config/autoload/navigation.global.php
+++ config/autoload/navigation.global.php
@@ -27,9 +27,27 @@ return array(
                 'label' => 'Contact',
                 'route' => 'contact',
             ),
-            array(
-                'label' => 'Users',
-                'route' => 'user',
+            'account' => array(
+                'label' => 'Account',
+                'route' => 'zfcuser',
+                'pages' => array(
+                    'home' => array(
+                        'label' => 'Dashboard',
+                        'route' => 'zfcuser',
+                    ),
+                    'login' => array(
+                        'label' => 'Sign In',
+                        'route' => 'zfcuser/login',
+                    ),
+                    'logout' => array(
+                        'label' => 'Sign Out',
+                        'route' => 'zfcuser/logout',
+                    ),
+                    'register' => array(
+                        'label' => 'Register',
+                        'route' => 'zfcuser/register',
+                    ),
+                ),
             ),
         ),
     ),
