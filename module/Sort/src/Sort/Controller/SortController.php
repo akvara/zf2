d136a5912b26a343c399c95b68fd2758b0ebf686
Andrius Kvaraciejus <akvara@gmail.com>
2013-10-06 01:19:30 +0300

    now

---
+++ module/Sort/src/Sort/Controller/SortController.php
@@ -0,0 +1,65 @@
+<?php
+namespace Sort\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Form\Annotation\AnnotationBuilder;
+use Sort\Model\SortableArea;
+
+class SortController extends AbstractActionController
+{
+
+    private function stringToArray($inputString, $capitalise) {
+
+        if ($capitalise) {
+            $inputString = strtoupper($inputString);
+        }
+        $inputString = str_replace("\n", ' ', $inputString);
+        $inputString = str_replace("\r", ' ', $inputString);
+        $sortable = explode (' ',  $inputString );
+        sort($sortable);
+        $sortable = array_diff($sortable, ['']);
+        $sortable = array_unique($sortable);
+
+        return $sortable;
+    }
+
+    public function indexAction()
+    {
+        $sortableArea    = new SortableArea();
+        $builder    = new AnnotationBuilder();
+        $form       = $builder->createForm($sortableArea);
+
+        $request = $this->getRequest();
+        if ($request->isPost()){
+            $form->bind($sortableArea);
+            $form->setData($request->getPost());
+
+            if ($form->isValid()){
+
+                $formData = $form->getData();
+
+                $sortableArrray = $this->stringToArray($formData->sortableText, $formData->capitalise=="1");
+                $comparableArrray = $this->stringToArray($formData->comparableText, $formData->capitalise=="1");
+
+                $sortableTextDiff = implode(' ', array_diff($sortableArrray,$comparableArrray));
+                $comparableTextDiff = implode(' ', array_diff($comparableArrray, $sortableArrray));
+                $sortedText = implode(' ', $sortableArrray);
+                $comparedText = implode(' ', $comparableArrray);
+                $form->setData(
+                    [
+                        'sortableText' => $sortedText,
+                        'comparableText' => $comparedText,
+                        'sortableTextDiff' => $sortableTextDiff,
+                        'comparableTextDiff' => $comparableTextDiff,
+                        'compare'=>"Compare"
+                    ]
+                );
+
+//                return ['form'=>$form];
+            }
+        }
+
+        return array('form'=>$form);
+    }
+
+}
---
+++ module/Sort/src/Sort/Model/SortableArea.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Sort\Model;
+
+use Zend\Form\Annotation;
+use Zend\Form\Element\Textarea;
+use Zend\Form\Element\Submit;
+
+
+/**
+ * @Annotation\Hydrator("Zend\Stdlib\Hydrator\ObjectProperty")
+ * @Annotation\Name("SortableArea")
+ * @Annotation\Attributes({"width": 100})
+
+ */
+class SortableArea
+{
+    /**
+
+    /**
+     * @Annotation\Type("TextArea")
+     * @Annotation\Required({"required":"true" })
+     * @Annotation\Filter({"name":"StripTags"})
+     * @Annotation\Attributes({"rows":"5"})
+     * @Annotation\Attributes({"cols":"5"})
+     * @Annotation\Options({"label":" "})
+     */
+    public $sortableText;
+
+    /**
+     * @Annotation\Type("Text")
+     * @Annotation\Options({"label":" "})
+//     * @Annotation\Attributes({"width": 100})
+     * @Annotation\Attributes({"disabled":"disabled"})
+     */
+    public $sortableTextDiff;
+
+    /**
+     * @Annotation\Type("TextArea")
+     * @Annotation\AllowEmpty
+     * @Annotation\Filter({"name":"StripTags"})
+     * @Annotation\Attributes({"rows":"5"})
+     * @Annotation\Options({"label":" "})
+     */
+    public $comparableText;
+
+//    /**
+//     * @Annotation\Type("Submit")
+//     * @Annotation\Attributes({"value":"Sort"})
+//     */
+//    public $sort;
+
+    /**
+     * @Annotation\Type("Text")
+     * @Annotation\Options({"label":" "})
+     * @Annotation\Attributes({"disabled":"disabled"})
+     */
+    public $comparableTextDiff;
+
+    /**
+     * @Annotation\Type("Zend\Form\Element\Checkbox")
+     * @Annotation\Options({"label":"Capitalise", "default":"1"})
+     */
+    public $capitalise;
+
+    /**
+     * @Annotation\Type("Zend\Form\Element\Checkbox")
+     * @Annotation\Options({"label":"Remove dupplicates", "value":"1"})
+     */
+    public $de_dupplicate;
+
+    /**
+     * @Annotation\Type("Submit")
+     * @Annotation\Attributes({"value":"Sort / Compare"})
+     */
+    public $compare;
+}
