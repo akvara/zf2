0bdeb29f39a62610e5d7dca31197170ac8ea95f5
Andrius Kvaraciejus <akvara@gmail.com>
2013-10-06 18:21:48 +0300

    Unit testing started

--- module/Album/test/AlbumTest/Controller/AlbumControllerTest.php
+++ module/Album/test/AlbumTest/Controller/AlbumControllerTest.php
@@ -2,16 +2,18 @@
 
 namespace AlbumTest\Controller;
 
-use AlbumTest\Bootstrap;
-use Album\Controller\AlbumController;
-use Zend\Http\Request;
-use Zend\Http\Response;
-use Zend\Mvc\MvcEvent;
-use Zend\Mvc\Router\RouteMatch;
-use Zend\Mvc\Router\Http\TreeRouteStack as HttpRouter;
-use PHPUnit_Framework_TestCase;
+use Test\Bootstrap;
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
 
-class AlbumControllerTest extends PHPUnit_Framework_TestCase
+//use Album\Controller\AlbumController;
+//use Zend\Http\Request;
+//use Zend\Http\Response;
+//use Zend\Mvc\MvcEvent;
+//use Zend\Mvc\Router\RouteMatch;
+//use Zend\Mvc\Router\Http\TreeRouteStack as HttpRouter;
+//use PHPUnit_Framework_TestCase;
+
+class AlbumControllerTest extends AbstractHttpControllerTestCase
 {
     protected $controller;
     protected $request;
@@ -19,83 +21,85 @@ class AlbumControllerTest extends PHPUnit_Framework_TestCase
     protected $routeMatch;
     protected $event;
 
-    protected function setUp()
+    public function setUp()
     {
-        $serviceManager = Bootstrap::getServiceManager();
-        $this->controller = new AlbumController();
-        $this->request    = new Request();
-        $this->routeMatch = new RouteMatch(array('controller' => 'index'));
-        $this->event      = new MvcEvent();
-        $config = $serviceManager->get('Config');
-        $routerConfig = isset($config['router']) ? $config['router'] : array();
-        $router = HttpRouter::factory($routerConfig);
-        $this->event->setRouter($router);
-        $this->event->setRouteMatch($this->routeMatch);
-        $this->controller->setEvent($this->event);
-        $this->controller->setServiceLocator($serviceManager);
+        $this->setApplicationConfig(Bootstrap::getApplicationConfig());
+
+        parent::setUp();
     }
 
-    public function testAddActionCanBeAccessed()
-    {
-        $this->routeMatch->setParam('action', 'add');
-
-        $result   = $this->controller->dispatch($this->request);
-        $response = $this->controller->getResponse();
-
-        $this->assertEquals(200, $response->getStatusCode());
-    }
-
-    public function testDeleteActionCanBeAccessed()
-    {
-        $this->routeMatch->setParam('action', 'delete');
-        $this->routeMatch->setParam('id', '1');
-
-        $result   = $this->controller->dispatch($this->request);
-        $response = $this->controller->getResponse();
-
-        $this->assertEquals(200, $response->getStatusCode());
-    }
-
-    public function testDeleteActionRedirect()
-    {
-        $this->routeMatch->setParam('action', 'delete');
-
-        $result   = $this->controller->dispatch($this->request);
-        $response = $this->controller->getResponse();
-
-        $this->assertEquals(302, $response->getStatusCode());
-    }
-
-    public function testEditActionCanBeAccessed()
-    {
-        $this->routeMatch->setParam('action', 'edit');
-        $this->routeMatch->setParam('id', '1');//Add this Row
-
-        $result   = $this->controller->dispatch($this->request);
-        $response = $this->controller->getResponse();
-
-        $this->assertEquals(200, $response->getStatusCode());
-    }
-    public function testEditActionRedirect()
-    {
-        $this->routeMatch->setParam('action', 'edit');
-
-        $result   = $this->controller->dispatch($this->request);
-        $response = $this->controller->getResponse();
-
-        $this->assertEquals(302, $response->getStatusCode());
-    }
     public function testIndexActionCanBeAccessed()
     {
-        $this->routeMatch->setParam('action', 'index');
+        $this->dispatch('/album');
+        $this->assertResponseStatusCode(200);
 
-        $result   = $this->controller->dispatch($this->request);
-        $response = $this->controller->getResponse();
+        $this->assertModuleName('Album');
+        $this->assertControllerName('Album\Controller\Album');
+        $this->assertControllerClass('AlbumController');
+        $this->assertMatchedRouteName('album');
+    }
 
-        $this->assertEquals(200, $response->getStatusCode());
-    }
-    public function testGetAlbumTableReturnsAnInstanceOfAlbumTable()
-    {
-        $this->assertInstanceOf('Album\Model\AlbumTable', $this->controller->getAlbumTable());
-    }
+//    public function testAddActionCanBeAccessed()
+//    {
+//        $this->routeMatch->setParam('action', 'add');
+//
+//        $result   = $this->controller->dispatch($this->request);
+//        $response = $this->controller->getResponse();
+//
+//        $this->assertEquals(200, $response->getStatusCode());
+//    }
+//
+//    public function testDeleteActionCanBeAccessed()
+//    {
+//        $this->routeMatch->setParam('action', 'delete');
+//        $this->routeMatch->setParam('id', '1');
+//
+//        $result   = $this->controller->dispatch($this->request);
+//        $response = $this->controller->getResponse();
+//
+//        $this->assertEquals(200, $response->getStatusCode());
+//    }
+//
+//    public function testDeleteActionRedirect()
+//    {
+//        $this->routeMatch->setParam('action', 'delete');
+//
+//        $result   = $this->controller->dispatch($this->request);
+//        $response = $this->controller->getResponse();
+//
+//        $this->assertEquals(302, $response->getStatusCode());
+//    }
+//
+//    public function testEditActionCanBeAccessed()
+//    {
+//        $this->routeMatch->setParam('action', 'edit');
+//        $this->routeMatch->setParam('id', '1');//Add this Row
+//
+//        $result   = $this->controller->dispatch($this->request);
+//        $response = $this->controller->getResponse();
+//
+//        $this->assertEquals(200, $response->getStatusCode());
+//    }
+//    public function testEditActionRedirect()
+//    {
+//        $this->routeMatch->setParam('action', 'edit');
+//
+//        $result   = $this->controller->dispatch($this->request);
+//        $response = $this->controller->getResponse();
+//
+//        $this->assertEquals(302, $response->getStatusCode());
+//    }
+//    public function testIndexActionCanBeAccessed()
+//    {
+//        $this->routeMatch->setParam('action', 'index');
+//
+//        $result   = $this->controller->dispatch($this->request);
+//        $response = $this->controller->getResponse();
+//
+//        $this->assertEquals(200, $response->getStatusCode());
+//    }
+//    public function testGetAlbumTableReturnsAnInstanceOfAlbumTable()
+//    {
+//        $this->assertInstanceOf('Album\Model\AlbumTable', $this->controller->getAlbumTable());
+//    }
 }
